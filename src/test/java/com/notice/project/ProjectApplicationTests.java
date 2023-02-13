c14cc89273424f8b8a6ae91d1c2923a4cfd786a3
mollani0704 <mollani0704@gmail.com>
2023-02-13 17:33:48 +0900

    [Add] project 초기 세팅

---
+++ src/test/java/com/notice/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notice.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
