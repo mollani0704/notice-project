483ac0642ccc5e048d251327b7e4ecff11d8d007
mollani0704 <mollani0704@gmail.com>
2023-04-12 16:17:46 +0900

    [Add] notice Page이동 기능 추가

--- src/main/resources/static/js/notice/notice.js
+++ src/main/resources/static/js/notice/notice.js
@@ -19,8 +19,13 @@ function load(nowPage) {
 		
 		dataType: "json",
 		success: (response) => {
-			console.log(response)
-			getList(response.data);
+			if(response.data != null) {			
+				getList(response.data);
+				getPageNumbers(response.data[0].totalNoticeCount);
+			} else {
+				getList(new Array());
+				getPageNumbers(0);
+			}
 		},
 		error: (error) => {
 			console.log(error);
@@ -41,10 +46,71 @@ function getList(list) {
 				<td>${notice.noticeCount}</td>
 			</tr>
 		`
+	});
+	
+	const noticeRows = document.querySelectorAll(".notice__row");
+	
+	noticeRows.forEach((row) => {
+		row.addEventListener('click',() => {
+			const noticeCode = row.querySelectorAll("td")[0].textContent;
+			location.href = "/notice/" + noticeCode;
+		})
 	})
 } 
 
-
+function getPageNumbers(totalNoticeCount) {
+	const totalPageCount = totalNoticeCount % 10 == 0 ? totalNoticeCount / 10 : (totalNoticeCount / 10) + 1;
+	let startIndex = nowPage % 5 == 0 ? nowPage - 4 : nowPage - (nowPage % 5) + 1;
+	let endIndex = startIndex + 4 <= totalPageCount ? startIndex + 4 : totalPageCount;
+	
+	const pageButtons = document.querySelector(".page__buttons");
+	
+	pageButtons.innerHTML = ``;
+	
+	if(startIndex != 1) {
+		pageButtons.innerHTML += `
+			<button type="button" class="page__button pre">&lt;</button>
+		`
+	}
+	
+	for(let i = startIndex; i <= endIndex; i++) {
+		pageButtons.innerHTML += `
+			<button type="button" class="page__button">${i}</button>
+		`
+	}
+	
+	if(startIndex != totalPageCount) {
+		pageButtons.innerHTML += `
+			<button type="button" class="page__button next">&gt;</button>
+		`
+	}
+	
+	if(startIndex != 1) {
+		const prePageButton = document.querySelector(".pre");
+		prePageButton.addEventListener('click', () => {
+			nowPage = startIndex - 1;
+			load(nowPage);
+		})
+	}
+	
+	if(startIndex != totalPageCount) {
+		const nextPageButton = document.querySelector('.next');
+		nextPageButton.addEventListener('click', () => {
+			nowPage = endIndex - 1;
+			load(nowPage);
+		})
+	}
+	
+	const pageNumberButtons = document.querySelectorAll(".page__button");
+	pageNumberButtons.forEach(button => {
+		if(button.textContent != "<" && button.textContent != ">") {
+			button.addEventListener('click', () => {
+				nowPage = button.textContent;
+				load(nowPage);
+			})
+		}
+	})
+}
 
 
 
